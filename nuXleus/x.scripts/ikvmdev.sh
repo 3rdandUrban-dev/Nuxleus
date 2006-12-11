265179fc624d9b903bba244290034b0f25df9a3f
m.david <m.david@83d55c6b-2440-4c65-b031-4153121aa5aa>
2006-12-11 01:17:46 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/nuXleus/x.scripts/ikvmdev.sh

---
+++ nuXleus/x.scripts/ikvmdev.sh
@@ -0,0 +1,3 @@
+cvs -z3 -d:pserver:anonymous@ikvm.cvs.sourceforge.net:/cvsroot/ikvm co -P ikvm
+cvs -z3 -d:pserver:anonymous@cvs.savannah.gnu.org:/sources/classpath co classpath
+mv classpath classpath-0.93
