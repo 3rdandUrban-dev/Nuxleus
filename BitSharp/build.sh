2b0ddb3834b5270b8b32ffb6a9a5797d89243512
m.david <m.david@83d55c6b-2440-4c65-b031-4153121aa5aa>
2006-12-12 06:16:33 +0000

    a few more pieces to the core nuxleus puzzle

---
+++ BitSharp/build.sh
@@ -0,0 +1,16 @@
+SVNREPO=http://svn.myrealbox.com/source/trunk/bitsharp/src/
+DIST=BitSharp
+VERSION=svn
+
+update () {
+  if [ ! -d $2 ]; then
+    svn checkout $1 $2
+  else
+    svn update $2
+  fi
+}
+
+echo 'Updating BitSharp'
+update $SVNREPO bitsharp
+
+svn export bitsharp/ BitSharp
