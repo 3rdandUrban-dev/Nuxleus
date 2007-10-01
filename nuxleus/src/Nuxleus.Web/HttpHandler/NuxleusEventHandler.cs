3e9476d93dc2d886bc358114aa40cd65a7438a74
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-10-01 16:31:05 +0000

    missing files

---
+++ nuxleus/src/Nuxleus.Web/HttpHandler/NuxleusEventHandler.cs
@@ -0,0 +1,33 @@
+using System;
+using System.IO;
+using System.Data;
+using System.Configuration;
+using System.Threading;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Collections;
+using System.Text;
+using System.Xml;
+using Nuxleus.Configuration;
+using System.Collections.Generic;
+using Nuxleus.Cryptography;
+
+namespace Nuxleus.Web.HttpHandler
+{
+    public class NuxleusEventHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+        }
+
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+    }
+}
