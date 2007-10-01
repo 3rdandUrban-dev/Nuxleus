7617da2997f2b999f1653386583e39a7dc371c8b
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-10-01 11:45:52 +0000

    for future and past code storage

--- nuxleus/src/FileSystemWatcher/Watcher.cs
+++ nuxleus/src/FileSystemWatcher/Watcher.cs
@@ -9,28 +9,28 @@ namespace Nuxleus
 {
     public class FileSystemWatcher
     {
-	static Watcher _fileSystemWatcher;
-        
+        static Watcher _fileSystemWatcher;
+
         public static void Main()
         {
-	    	string[] args = System.Environment.GetCommandLineArgs();
+            string[] args = System.Environment.GetCommandLineArgs();
 
-            	if (args.Length != 2)
-            	{
-                	Console.WriteLine("Usage: Watcher.exe (directory)");
-                	return;
-            	}
-            	Console.WriteLine(args[1]);
-            	_fileSystemWatcher = new Watcher(args[1], "", Console.Out);
-		Run(_fileSystemWatcher);
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: Watcher.exe (directory)");
+                return;
+            }
+            Console.WriteLine(args[1]);
+            //_fileSystemWatcher = new Watcher(args[1], "", Console.Out);
+            Run(_fileSystemWatcher = new Watcher(args[1], "", Console.Out));
         }
 
         [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
         public static void Run(Watcher fileSystemWatcher)
-	{
-		fileSystemWatcher.Watch(true);
-            	Console.WriteLine("Press \'q\' to quit the sample.");
-            	while (Console.Read() != 'q') ;
+        {
+            fileSystemWatcher.Watch(true);
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            while (Console.Read() != 'q') ;
         }
 
     }
