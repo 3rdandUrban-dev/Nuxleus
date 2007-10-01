ca68d83bec6809e459725865e4c506b6c2780808
sylvain.hellegouarch <sylvain.hellegouarch@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-10-01 05:15:41 +0000

    Fixed the signature

--- nuxleus/src/Nuxleus.Authentication/Facebook.cs
+++ nuxleus/src/Nuxleus.Authentication/Facebook.cs
@@ -6,9 +6,23 @@ using System.Net;
 
 namespace Nuxleus.Authentication {
   public class Facebook {
-    public static void Authenticate(string email, string password) {
+    /// <summary>
+    /// Authenticate a user to Facebook allowing for  usage of the Facebook API.
+    /// </summary>
+    /// <remarks>
+    ///   <para>
+    ///     The authentication uses SSL to encrypt the communication with the 
+    ///     Facebook servers.
+    ///   </para>
+    /// </remarks>
+    /// <param name="email">User email address registered on Facebook</param>
+    /// <param name="password">Clear text password for the user.</param>
+    /// <param name="authToken">Token returned by the call at auth.createToken </param>
+    /// <param name="apiKey">Public key to the application the user wishes to access</param>
+    /// <returns>Does not return anything.</returns>
+    public static void Authenticate(string email, string password, string authToken, string apiKey) {
       // First we grab the needed cookies to perform the login
-      string loginUri = string.Format ("http://www.facebook.com/login.php?api_key={0}&v=1.0", util.ApiKey)
+      string loginUri = string.Format ("http://www.facebook.com/login.php?api_key={0}&v=1.0&auth_token={1}", apiKey, authToken)
       HttpWebRequest r0 = (HttpWebRequest)WebRequest.Create(loginUri);
       r0.UserAgent = "Mozilla/5.0 (compatible)"; // we have to pretend what we're not otherwise Facebook is not happy 
       r0.CookieContainer = new CookieContainer();
@@ -21,7 +35,7 @@ namespace Nuxleus.Authentication {
       r1.ContentType = "application/x-www-form-urlencoded";
       r1.CookieContainer = new CookieContainer();
       r1.CookieContainer.Add(s0.Cookies);
-      string data = String.Format("version=1.0&auth_token={0}&api_key={1}&email={2}&pass={3}&persistent=1", auth_token, util.ApiKey, HttpUtility.UrlEncode(email), HttpUtility.UrlEncode(password));
+      string data = String.Format("version=1.0&auth_token={0}&api_key={1}&email={2}&pass={3}&persistent=1", authToken, apiKey, HttpUtility.UrlEncode(email), HttpUtility.UrlEncode(password));
       byte[] bytes = Encoding.ASCII.GetBytes(data);
       r1.ContentLength = bytes.Length;
       Stream body = r1.GetRequestStream();
