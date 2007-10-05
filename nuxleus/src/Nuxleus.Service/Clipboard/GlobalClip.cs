415f5f55b065515cfd1a43f9eeca68046a906984
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-10-05 03:39:14 +0000

    this now builds properly

--- nuxleus/src/Nuxleus.Service/Clipboard/GlobalClip.cs
+++ nuxleus/src/Nuxleus.Service/Clipboard/GlobalClip.cs
@@ -1,14 +1,10 @@
 using System;
 using Nuxleus.Configuration;
 
-[assembly: CLSCompliant(true)]
 namespace Nuxleus.Service
 {
-
     public partial class GlobalClip
     {
-
         public GlobalClip() { }
-
     }
 }
