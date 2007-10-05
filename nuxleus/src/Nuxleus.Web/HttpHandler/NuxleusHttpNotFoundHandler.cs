261a0a434b930073d7d57f4c70a12c8413aee436
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-10-05 04:12:35 +0000

    moving into Scripts.  Need to do this in two stages

--- nuxleus/src/Nuxleus.Web/HttpHandler/NuxleusHttpNotFoundHandler.cs
+++ nuxleus/src/Nuxleus.Web/HttpHandler/NuxleusHttpNotFoundHandler.cs
@@ -7,80 +7,94 @@ using System.Web;
 using Nuxleus.Utility;
 
 namespace Nuxleus.Web.HttpHandler
-{ 
-  public class NuxleusHttpNotFoundHandler : IHttpHandler
-  {  
-    private void FindMatch(string current, IList<string> matches, string[] tokens, int index) { 
-      if(index == tokens.Length) {
-	return;
-      }
-
-      FileSystemInfo[] directories = (new DirectoryInfo(current)).GetFileSystemInfos();
-
-      string token = tokens[index].ToLower();
-      foreach (FileSystemInfo fsi in directories)  {
-	string name = fsi.Name.ToLower();
-	if(fsi is DirectoryInfo) {
-	  DirectoryInfo dir = (DirectoryInfo)fsi;
-	  if(name.Contains(token)) {
-	    if(matches.Contains(dir.Parent.FullName))
-	      matches.Remove(dir.Parent.FullName);
-	    matches.Add(dir.FullName);
-	    FindMatch(dir.FullName, matches, tokens, index+1);
-	  } else if (StringMatching.AreNeighbors(token, name, 2)) {
-	    if(matches.Contains(dir.Parent.FullName))
-	      matches.Remove(dir.Parent.FullName);
-	    matches.Add(dir.FullName);
-	    FindMatch(dir.FullName, matches, tokens, index+1);
-	  }
-	} else if(fsi is FileInfo) {
-	  FileInfo fi = (FileInfo)fsi;
-	  if(name.Contains(token)) {
-	    if(matches.Contains(fi.DirectoryName))
-	      matches.Remove(fi.DirectoryName);
-	    matches.Add(fi.FullName);
-	  } else if (StringMatching.AreNeighbors(token, name, 2)) {
-	    if(matches.Contains(fi.DirectoryName))
-	      matches.Remove(fi.DirectoryName);
-	    matches.Add(fi.FullName);
-	  }
-	}
-      }
-    }
-    
-    public void ProcessRequest(HttpContext context)
+{
+    public class NuxleusHttpNotFoundHandler : IHttpHandler
     {
-      HttpRequest req = context.Request;
-      HttpResponse resp = context.Response;
+        private void FindMatch(string current, IList<string> matches, string[] tokens, int index)
+        {
+            if (index == tokens.Length)
+            {
+                return;
+            }
 
-      IList<string> matches = new List<string>();
-      string[] tokens = req.Path.Trim('/').ToLower().Split('/');
-      string current = Directory.GetCurrentDirectory();
-      FindMatch(current, matches, tokens, 0);
+            FileSystemInfo[] directories = (new DirectoryInfo(current)).GetFileSystemInfos();
 
-      StringBuilder sb = new StringBuilder(String.Format("<html><head/><body><p>Could not find {0}.</p>", HttpUtility.HtmlEncode(req.Path)));
-      
-      if(matches.Count != 0) {
-	sb.Append("<p>We have however found potential match:</p><ul>");
-	foreach(string m in matches) {
-	  sb.Append(String.Format("<li>{0}</li>", HttpUtility.HtmlEncode(m)));
-	}
-	
-	sb.Append("</ul>");
-      }
-      sb.Append("<p>Or maybe you'd rather create that resource instead?</p></body></html>");
-      
-      resp.Write(sb.ToString());
+            string token = tokens[index].ToLower();
+            foreach (FileSystemInfo fsi in directories)
+            {
+                string name = fsi.Name.ToLower();
+                if (fsi is DirectoryInfo)
+                {
+                    DirectoryInfo dir = (DirectoryInfo)fsi;
+                    if (name.Contains(token))
+                    {
+                        if (matches.Contains(dir.Parent.FullName))
+                            matches.Remove(dir.Parent.FullName);
+                        matches.Add(dir.FullName);
+                        FindMatch(dir.FullName, matches, tokens, index + 1);
+                    }
+                    else if (StringMatching.AreNeighbors(token, name, 2))
+                    {
+                        if (matches.Contains(dir.Parent.FullName))
+                            matches.Remove(dir.Parent.FullName);
+                        matches.Add(dir.FullName);
+                        FindMatch(dir.FullName, matches, tokens, index + 1);
+                    }
+                }
+                else if (fsi is FileInfo)
+                {
+                    FileInfo fi = (FileInfo)fsi;
+                    if (name.Contains(token))
+                    {
+                        if (matches.Contains(fi.DirectoryName))
+                            matches.Remove(fi.DirectoryName);
+                        matches.Add(fi.FullName);
+                    }
+                    else if (StringMatching.AreNeighbors(token, name, 2))
+                    {
+                        if (matches.Contains(fi.DirectoryName))
+                            matches.Remove(fi.DirectoryName);
+                        matches.Add(fi.FullName);
+                    }
+                }
+            }
+        }
 
-      // Even if we build a friendly page, this is still a Page Not Found
-      // let's not break HTTP
-      resp.StatusCode = 404;
-      resp.StatusDescription = "Not Found";
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest req = context.Request;
+            HttpResponse resp = context.Response;
+
+            IList<string> matches = new List<string>();
+            string[] tokens = req.Path.Trim('/').ToLower().Split('/');
+            string current = Directory.GetCurrentDirectory();
+            FindMatch(current, matches, tokens, 0);
+
+            StringBuilder sb = new StringBuilder(String.Format("<html><head/><body><p>Could not find {0}.</p>", HttpUtility.HtmlEncode(req.Path)));
+
+            if (matches.Count != 0)
+            {
+                sb.Append("<p>We have however found potential match:</p><ul>");
+                foreach (string m in matches)
+                {
+                    sb.Append(String.Format("<li>{0}</li>", HttpUtility.HtmlEncode(m)));
+                }
+
+                sb.Append("</ul>");
+            }
+            sb.Append("<p>Or maybe you'd rather create that resource instead?</p></body></html>");
+
+            resp.Write(sb.ToString());
+
+            // Even if we build a friendly page, this is still a Page Not Found
+            // let's not break HTTP
+            resp.StatusCode = 404;
+            resp.StatusDescription = "Not Found";
+        }
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
     }
-
-    public bool IsReusable
-    {
-      get { return false; }
-    }
-  }
 }
