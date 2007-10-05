31768c205d8a32a0fff30d61102e6497a9e912da
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-10-05 00:24:19 +0000

    for testing the SOAP service

---
+++ nuxleus/src/Nuxleus/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nuxleus.Amp.Fm;
+using System.Collections;
+using Nuxleus;
+using System.Xml;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Search search = new Search();
+            object[] searchResults = search.SearchArtist("pearl jam");
+            
+            IEnumerator entityArray = searchResults.GetEnumerator();
+
+            while (entityArray.MoveNext())
+            {
+                object[] entity = (object[])entityArray.Current;
+                XmlElement term = (XmlElement)entity[1];
+                XmlElement label = (XmlElement)entity[2];
+                XmlElement scheme = (XmlElement)entity[3];
+                Console.WriteLine("{0}: {1}{2}", label.InnerText, scheme.InnerText, term.InnerText);
+            }
+        }
+    }
+}
