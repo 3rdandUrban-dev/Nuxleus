38823412d3401982bad764b40778176876f18945
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-10-06 00:57:26 +0000

    the new command line util

---
+++ nuxleus/src/Nuxleus.Command/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml;
+using System.Collections;
+using Nuxleus;
+using Nuxleus.Amp.Fm;
+
+namespace Nuxleus
+{
+    public class Command
+    {
+        static void Main(string[] args)
+        {
+            Search search = new Search();
+            object[] searchResults = search.SearchArtist(args[0]);
+
+            IEnumerator entityArray = searchResults.GetEnumerator();
+
+            while (entityArray.MoveNext())
+            {
+                object[] entity = (object[])entityArray.Current;
+                Console.WriteLine
+                        ("{0}: {1}{2}",
+                        ((XmlElement)entity[2]).InnerText,
+                        ((XmlElement)entity[3]).InnerText,
+                        ((XmlElement)entity[1]).InnerText
+                        );
+            }
+        }
+    }
+}
