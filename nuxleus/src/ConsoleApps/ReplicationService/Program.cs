0ebe570c62d86665cdf866728ba68f7917de75be
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-10-07 21:56:43 +0000

    initial check-in of refactoring

---
+++ nuxleus/src/ConsoleApps/ReplicationService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Reflection;
+using Nuxleus.Service;
+using System.ServiceProcess;
+using System.Runtime.Remoting;
+
+namespace Nuxleus.Service
+{
+    public class Program
+    {
+
+        // The main entry point for the process
+        static void Main(string[] args)
+        {
+
+            ServiceBase[] ServicesToRun;
+
+            ServicesToRun = new ServiceBase[] { new FileSystemWatcherService("foo") };
+
+            ServiceBase.Run(ServicesToRun);
+
+        }
+    }
+}
--- nuxleus/src/Nuxleus.Service/FileSystem/Replication/Replication.cs
+++ nuxleus/src/Nuxleus.Service/FileSystem/Replication/Replication.cs
@@ -1,58 +1,220 @@
-﻿// Turn on logging to the event log.
-#define LOGEVENTS
-
-using System;
-using System.IO;
-using System.Threading;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.ServiceProcess;
+using System.IO;
+using System.Threading;
+using System.Reflection;
 using System.Text;
-using Microsoft.Win32;
-using System.Runtime.InteropServices;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Messaging;
+using System.Configuration.Install;
 
 namespace Nuxleus.Service
 {
-
-    public enum ReplicatonServiceCustomCommands { StopWorker = 128, RestartWorker, CheckWorker };
-
-    [StructLayout(LayoutKind.Sequential)]
-    public struct SERVICE_STATUS
-    {
-        public int serviceType;
-        public int currentState;
-        public int controlsAccepted;
-        public int win32ExitCode;
-        public int serviceSpecificExitCode;
-        public int checkPoint;
-        public int waitHint;
-    }
-
-    public enum State
-    {
-        SERVICE_STOPPED = 0x00000001,
-        SERVICE_START_PENDING = 0x00000002,
-        SERVICE_STOP_PENDING = 0x00000003,
-        SERVICE_RUNNING = 0x00000004,
-        SERVICE_CONTINUE_PENDING = 0x00000005,
-        SERVICE_PAUSE_PENDING = 0x00000006,
-        SERVICE_PAUSED = 0x00000007,
-    }
-
     public class ReplicationService : ServiceBase
     {
-        private SERVICE_STATUS replicatinServiceStatus;
-
-        private Thread workerThread = null;
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private Container components = null;
 
         public ReplicationService()
         {
-            CanPauseAndContinue = true;
-            //CanHandleSessionChangeEvent = true;
-            ServiceName = "ReplicationService";
+            // This call is required by the Windows.Forms Component Designer.
+            InitializeComponent();
         }
 
+        // The main entry point for the process
+        public static void Main(object[] args)
+        {
+            if (args.Length == 1 && (string)args[0] == "-standalone")
+            {
+                ReplicationService svc = new ReplicationService();
+
+                svc.OnStart(null);
+
+                Thread.Sleep(3000);
+
+                svc.OnStop();
+            }
+            else
+            {
+                ServiceBase[] ServicesToRun;
+
+                ServicesToRun = new ServiceBase[] { new ReplicationService() };
+
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new Container();
+            this.ServiceName = "Replication Service";
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Set things in motion so your service can do its work.
+        /// </summary>
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                Log.Write("Starting Service...");
+            }
+            catch (Exception ex)
+            {
+                Log.Write(ex);
+            }
+        }
+
+        /// <summary>
+        /// Stop this service.
+        /// </summary>
+        protected override void OnStop()
+        {
+            try
+            {
+                Log.Write("Stopping Service...");
+                this.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Log.Write(ex);
+            }
+        }
+
+
+
+    }
+
+    public sealed class Log
+    {
+        private static string _logPath;
+
+        static Log()
+        {
+            _logPath = Path.Combine(
+                Path.GetDirectoryName(typeof(Log).Assembly.Location),
+                "Test.log");
+        }
+
+        public static void Write(string msg)
+        {
+            using (FileStream fs = new FileStream(_logPath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
+            using (StreamWriter w = new StreamWriter(fs, System.Text.Encoding.Default))
+            {
+                w.WriteLine("{0:yyyy-MM-dd HH:mm:ss} - {1}", DateTime.Now, msg);
+                w.Flush();
+            }
+        }
+
+        public static void Write(Exception ex)
+        {
+            StringBuilder message = new StringBuilder();
+            StringBuilder stacktrace = new StringBuilder();
+
+            for (Exception e = ex; e != null; e = e.InnerException)
+            {
+                if (message.Length > 0)
+                    message.Append(Environment.NewLine);
+
+                message.Append(e.Message);
+
+                if (stacktrace.Length > 0)
+                {
+                    stacktrace.Append(Environment.NewLine);
+                    stacktrace.Append("----");
+                }
+                stacktrace.Append(e.StackTrace);
+            }
+
+            Write(message.ToString() + Environment.NewLine + stacktrace.ToString());
+        }
+    }
+
+
+
+    [RunInstaller(true)]
+    public class ProjectInstaller : Installer
+    {
+        private ServiceProcessInstaller serviceProcessInstaller1;
+        private ServiceInstaller serviceInstaller1;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private Container components = null;
+
+        public ProjectInstaller()
+        {
+            // This call is required by the Designer.
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Component Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new ServiceProcessInstaller();
+            this.serviceInstaller1 = new ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.DisplayName = "nuXleus File Replication Service";
+            this.serviceInstaller1.ServiceName = "Log.Write";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+																					  this.serviceProcessInstaller1,
+																					  this.serviceInstaller1});
+
+        }
+        #endregion
     }
 }
