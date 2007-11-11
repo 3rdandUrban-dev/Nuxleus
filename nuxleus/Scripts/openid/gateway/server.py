869801d7faaa4f0374c64b9dab0357fda8a96a93
sylvain.hellegouarch <sylvain.hellegouarch@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-11-11 17:42:15 +0000

    openid gateway import

---
+++ nuxleus/Scripts/openid/gateway/server.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import cherrypy
+from openidgateway import make_openidgateway
+
+if __name__ == '__main__':
+    global_conf = {'engine.autoreload_on' : False,
+                   'server.socket_port' : 7600, 
+                   'server.socket_host': '127.0.0.1',
+                   'server.socket_queue_size': 25,
+                   'log.screen': False,
+                   'log.access_file': './access.log',
+                   'log.error_file': './error.log',
+                   'checker.on': False,}
+
+    cherrypy.config.update(global_conf)
+    cherrypy.checker.on = False
+
+    app_conf = {
+        'debug': True,
+        'base_url': 'http://gateway.openid.amp.fm/',
+        'xsltemplate_namespace': 'http://amp.fm/python/xsltemplates/',
+    }
+    
+    app = make_openidgateway(app_conf)
+    cherrypy.tree.graft(app, script_name='/')
+
+    class Dummy(object):
+        pass
+
+    cherrypy.quickstart(Dummy(), '/dummy')
+    #cherrypy.server.quickstart()
+    #try:
+    #    cherrypy.engine.start()
+    #except KeyboardInterrupt:
+    #    cherrypy.engine.stop()
+
+    
