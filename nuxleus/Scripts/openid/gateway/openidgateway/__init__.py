239bfcd34a3bf94a0f2ec89c5a05fde1d06ab1ce
ionrock <ionrock@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-11-12 08:23:44 +0000

    Removed the 400 code when no uname an return_location are passed along with the forms.

--- nuxleus/Scripts/openid/gateway/openidgateway/__init__.py
+++ nuxleus/Scripts/openid/gateway/openidgateway/__init__.py
@@ -63,7 +63,11 @@ class OpenIdGateway(object):
         headers = [('Content-Type', 'application/xml')]
         if not (req.params.get('uname') or req.params.get('return_location')):
             message ='There must be a uname and return_location in the query string'
-            raise HTTPBadRequest(detail=message)
+            params['message'] = message
+            params['status'] = 'failure'
+            set_params(environ, params)
+            start_response('200 OK', headers)
+            return []
         
         openid_url = req.params['uname']
         sess[self.ekey]['return_location'] = req.params['return_location']
