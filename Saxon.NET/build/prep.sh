d7b1768e33c5f6e3a17ce06082427ab1267e4d75
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-11-19 01:53:51 +0000

    don't need these

--- Saxon.NET/build/prep.sh
+++ Saxon.NET/build/prep.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 rm -rf Saxon/net/sf/saxon/ant
 find Saxon/ -name *.java > allsources.lst
-patch Saxon/net/sf/saxon/number/Numberer_fr.java < Numberer_fr.java.patch
-patch Saxon/net/sf/saxon/number/Numberer_de.java < ./Numberer_de.java.patch
