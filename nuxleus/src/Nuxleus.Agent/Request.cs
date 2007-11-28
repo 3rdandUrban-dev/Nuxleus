66fb304ea7ece7abac4968e2784010b222e31660
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-11-28 02:05:04 +0000

    missed these

---
+++ nuxleus/src/Nuxleus.Agent/Request.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+
+namespace Nuxleus.Agent
+{
+    public delegate void AsyncRequestCallback(Response response);
+
+    public struct Request
+    {
+        Guid _id;
+        string _message;
+        DateTime _timeStamp;
+        bool _isComplete;
+        Response _response;
+
+        public Request(string message) 
+        {
+            _id = Guid.NewGuid();
+            _message = message;
+            _timeStamp = DateTime.Now;
+            _isComplete = false;
+            _response = new Response();
+        }
+
+        public void BeginRequest()
+        {
+            ProcessRequest(new AsyncRequestCallback(CompleteRequest));
+        }
+
+        private void ProcessRequest(AsyncRequestCallback callback)
+        {
+            ///TODO: Build out request processing logic
+            Response myResponse = new Response();
+            myResponse.Message = "<foo>bar</foo>";
+            myResponse.Timestamp = DateTime.Now;
+            callback(myResponse);
+        }
+
+        public void CompleteRequest(Response response)
+        {
+            _isComplete = true;
+            _response = response;
+        }
+
+        public Guid ID { get { return _id; } }
+        public string Message { get { return _message; } }
+        public DateTime Timestamp { get { return _timeStamp; } set { _timeStamp = value; } }
+        public Response GetResponse { get { return _response; } }
+        public bool IsCompleted { get { return _isComplete; } }
+    }
+}
---
+++ nuxleus/src/Nuxleus.Agent/Response.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+
+namespace Nuxleus.Agent
+{
+    public struct Response
+    {
+        Guid _id;
+        string _message;
+        DateTime _timeStamp;
+
+        public Guid ID { get { return _id; } }
+        public string Message { get { return _message; } set { _message = value; } }
+        public DateTime Timestamp { get { return _timeStamp; } set { _timeStamp = value; } }
+    }
+}
