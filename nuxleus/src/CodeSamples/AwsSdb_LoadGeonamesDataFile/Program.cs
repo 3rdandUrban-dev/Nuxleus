f767c51c6deebffa157953cd11d96ca591935c06
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-12-21 14:20:06 +0000

    util for loading the GeoNames DB into SDB

---
+++ nuxleus/src/CodeSamples/AwsSdb_LoadGeonamesDataFile/Program.cs
@@ -0,0 +1,109 @@
+/* -*- Mode: Java; c-basic-offset: 2 -*- */
+/*
+ * This software code is made available "AS IS" without warranties of any
+ * kind.  You may copy, display, modify and redistribute the software
+ * code either by itself or as incorporated into your code; provided that
+ * you do not remove any proprietary notices.  Your use of this software
+ * code is at your own risk and you waive any claim against Amazon
+ * Web Services LLC or its affiliates with respect to your use of
+ * this software code.
+ * 
+ * @copyright 2007 Amazon Web Services LLC or its affiliates.
+ *            All rights reserved.
+ */
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using Nuxleus.Extension.Aws;
+using Nuxleus.Extension.Aws.Sdb;
+
+class BasicSample
+{
+
+    public static void Main (string[] args)
+    {
+        string awsAccessKey =
+          System.Environment.GetEnvironmentVariable("SDB_ACCESS_KEY");
+        string awsSecretKey =
+          System.Environment.GetEnvironmentVariable("SDB_SECRET_KEY");
+
+        string domainName = "geonames";
+
+        // Create a new instance of the SDB class
+        HttpQueryConnection connection = new HttpQueryConnection(awsAccessKey, awsSecretKey, "http://sdb.amazonaws.com/");
+        Sdb sdb = new Sdb(connection);
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("Step 1: Creating the domain.");
+
+        try
+        {
+            sdb.CreateDomain(domainName);
+        }
+        catch (SdbException ex)
+        {
+            handleException(ex);
+        }
+
+        Domain domain = sdb.GetDomain(domainName);
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("Step 2: Loading the GeoNames Data File.");
+
+        using (StreamReader csvReader = new StreamReader("./allCountries.txt"))
+        {
+            string inputLine = "";
+
+            while ((inputLine = csvReader.ReadLine()) != null)
+            {
+
+                string[] inputArray = inputLine.Split(new char[] { '\u0009' });
+                IEnumerator alternateNames = ((string)inputArray.GetValue(3)).Split(new char[] { ',' }).GetEnumerator();
+
+                System.Console.WriteLine(String.Format("Loading Item: {0}, with Place Name: {1}", (string)inputArray.GetValue(0), (string)inputArray.GetValue(1)));
+
+                Item item = domain.GetItem((string)inputArray.GetValue(0));
+
+                ArrayList attributes = new ArrayList();
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("name", (string)inputArray.GetValue(1)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("asciiname", (string)inputArray.GetValue(2)));
+
+                while (alternateNames.MoveNext())
+                {
+                    attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("alternatenames", (string)alternateNames.Current));
+                }
+
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("latitude", (string)inputArray.GetValue(4)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("longitude", (string)inputArray.GetValue(5)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("feature_class", (string)inputArray.GetValue(6)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("feature_code", (string)inputArray.GetValue(7)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("country_code", (string)inputArray.GetValue(8)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("cc2", (string)inputArray.GetValue(9)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("admin1_code", (string)inputArray.GetValue(10)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("admin2_code", (string)inputArray.GetValue(11)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("admin3_code", (string)inputArray.GetValue(12)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("admin4_code", (string)inputArray.GetValue(13)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("population", (string)inputArray.GetValue(14)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("elevation", (string)inputArray.GetValue(15)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("gtopo30", (string)inputArray.GetValue(16)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("timezone", (string)inputArray.GetValue(17)));
+                attributes.Add(new Nuxleus.Extension.Aws.Sdb.Attribute("modification_date", (string)inputArray.GetValue(1)));
+
+                try
+                {
+                    item.PutAttributes(attributes);
+                }
+                catch (SdbException ex)
+                {
+                    handleException(ex);
+                }
+            }
+        }
+    }
+
+    static void handleException (SdbException ex)
+    {
+        System.Console.WriteLine("Failure: {0}: {1} ({2})", ex.ErrorCode, ex.Message, ex.RequestId);
+    }
+}
