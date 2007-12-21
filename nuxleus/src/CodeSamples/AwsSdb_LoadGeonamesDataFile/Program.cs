c9a0fc16ca85436e4f6bb9e7993ae399030e56f8
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2007-12-21 20:55:09 +0000

    added copyright info

--- nuxleus/src/CodeSamples/AwsSdb_LoadGeonamesDataFile/Program.cs
+++ nuxleus/src/CodeSamples/AwsSdb_LoadGeonamesDataFile/Program.cs
@@ -1,5 +1,7 @@
-/* -*- Mode: Java; c-basic-offset: 2 -*- */
-/*
+/* 
+ * This software is derived from samples provided by Amazon Web Services.
+ * The original copyright notice for this sample code follows.
+ **************************************************************************
  * This software code is made available "AS IS" without warranties of any
  * kind.  You may copy, display, modify and redistribute the software
  * code either by itself or as incorporated into your code; provided that
@@ -10,6 +12,10 @@
  * 
  * @copyright 2007 Amazon Web Services LLC or its affiliates.
  *            All rights reserved.
+ * *************************************************************************
+ * Extensions to this code base are Copyright (c) 2006 by M. David Peterson
+ * The code contained in this file is licensed under a Creative Commons (Attribution 3.0) license
+ * Please see http://creativecommons.org/licenses/by/3.0/us/ for specific detail.
  */
 using System;
 using System.IO;
