5d87723942f643d252f7512aad1b69b8004a0c44
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2008-01-19 12:19:55 +0000

    moving to proper location

--- nuxleus/Web/Development/js/ie.js
+++
@@ -1,3 +0,0 @@
-function hello(){
-  alert('It seems you are running IE. Your code base has been optimized accordingly.');
-};
--- nuxleus/Web/Development/js/opera.js
+++
@@ -1,3 +0,0 @@
-function hello(){
-  alert('It seems you are running Opera. Your code base has been optimized accordingly.');
-};
