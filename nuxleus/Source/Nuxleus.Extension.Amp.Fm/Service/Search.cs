0d64059fc9821923d20bd6040f6ffac130e0cd22
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2008-03-11 08:00:15 +0000

    various updates from the private development repository

--- nuxleus/Source/Nuxleus.Extension.Amp.Fm/Service/Search.cs
+++ nuxleus/Source/Nuxleus.Extension.Amp.Fm/Service/Search.cs
@@ -7,71 +7,62 @@ using System.Web;
 using Nuxleus.Entity;
 using System.Xml.Serialization;
 
-namespace Nuxleus.Extension.Amp.Fm
-{
+namespace Nuxleus.Extension.Amp.Fm {
     /// <summary>
     /// Summary description for Search service
     /// </summary>
-    [WebService(Namespace = "http://amp.fm/service")]
-    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-    public class Search : WebService
-    {
+    [WebService(Namespace="http://amp.fm/service")]
+    [WebServiceBinding(ConformsTo=WsiProfiles.BasicProfile1_1)]
+    public class Search : WebService {
+
         public enum TYPE { TITLE, ARTIST, GENRE, LYRICS }
 
         [WebMethod(
-            Description = "Returns a list of media file titles that contain the specified search phrase",
-            EnableSession = false)]
+            Description="Returns a list of media file titles that contain the specified search phrase",
+            EnableSession=false)]
         [XmlInclude(typeof(Entity.Entity))]
-        public ArrayList SearchTitle(string searchPhrase, params string[] additionalSearchParams)
-        {
+        public Entity.Entity[] SearchTitle ( string searchPhrase, params string[] additionalSearchParams ) {
             return search(searchPhrase, TYPE.TITLE);
         }
 
         [WebMethod(
-            Description = "Returns a list of artists that contain the specified search phrase",
-            EnableSession = false)]
+            Description="Returns a list of artists that contain the specified search phrase",
+            EnableSession=false)]
         [XmlInclude(typeof(Entity.Entity))]
-        public ArrayList SearchArtist(string artistName)
-        {
+        public Entity.Entity[] SearchArtist ( string artistName ) {
             return search(artistName, TYPE.ARTIST);
         }
 
         [WebMethod(
-            Description = "Returns a list of genres that contain the specified search phrase",
-            EnableSession = false)]
+            Description="Returns a list of genres that contain the specified search phrase",
+            EnableSession=false)]
         [XmlInclude(typeof(Entity.Entity))]
-        public ArrayList SearchGenre(string genre)
-        {
+        public Entity.Entity[] SearchGenre ( string genre ) {
             return search(genre, TYPE.GENRE);
         }
 
         [WebMethod(
-            Description = "Returns a list of media files whos associated lyrics contain the specified search phrase",
-            EnableSession = false)]
+            Description="Returns a list of media files whos associated lyrics contain the specified search phrase",
+            EnableSession=false)]
         [XmlInclude(typeof(Entity.Entity))]
-        public ArrayList SearchLyrics(string searchPhrase)
-        {
+        public Entity.Entity[] SearchLyrics ( string searchPhrase ) {
             return search(searchPhrase, TYPE.LYRICS);
         }
 
         [WebMethod]
         [XmlInclude(typeof(Entity.Entity))]
-        private ArrayList search(string searchPhrase, TYPE type)
-        {
-            ArrayList entityList = new ArrayList();
+        private Entity.Entity[] search ( string searchPhrase, TYPE type ) {
+
             string scheme = "http://amp.fm/";
-            Entity.Entity entity0 = new Entity.Entity("search", searchPhrase, scheme);
-            Entity.Entity entity1 = new Entity.Entity("pearljam", "Pearl Jam", scheme);
-            Entity.Entity entity2 = new Entity.Entity("foobarfighters", "FooBar Fighters", scheme);
-            Entity.Entity entity3 = new Entity.Entity("elliottesmith", "Elliotte Smith", scheme);
 
-            entityList.Add(entity0);
-            entityList.Add(entity1);
-            entityList.Add(entity2);
-            entityList.Add(entity3);
+            Entity.Entity[] entityList = new Entity.Entity[] {
+                new Entity.Entity("search", searchPhrase, scheme),
+                new Entity.Entity("pearljam", "Pearl Jam", scheme),
+                new Entity.Entity("foobarfighters", "FooBar Fighters", scheme),
+                new Entity.Entity("elliottesmith", "Elliotte Smith", scheme),
+            };
 
-            switch (type)
-            {
+            switch (type) {
                 case TYPE.ARTIST:
                     return entityList;
                     break;
