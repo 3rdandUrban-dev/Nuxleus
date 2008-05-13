e7bb6e733b08aeddb59da8ca8cfa36388cdbeba3
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2008-05-13 22:45:16 +0000

    added basic process for invoking Zorba

---
+++ nuxleus/Source/Nuxleus.Process/Process/ZorbaProcess.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Security.Permissions;
+
+namespace Nuxleus.Process
+{
+    public class ZorbaProcess : System.Diagnostics.Process
+    {
+        TextWriter m_logWriter;
+        ZorbaProcess m_proc;
+        StreamReader m_output;
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public ZorbaProcess()
+        {
+            base.StartInfo.FileName = "zorba";
+
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public ZorbaProcess(string parameters, TextWriter logWriter)
+        {
+            m_logWriter = logWriter;
+            m_proc = new ZorbaProcess();
+            m_proc.StartInfo.FileName = "zorba";
+
+        }
+
+        public TextWriter LogWriter { get { return m_logWriter; } set { m_logWriter = value; } }
+
+
+        public void RunQuery(string fullPath)
+        {
+            this.StartInfo.Arguments = "to be added";
+            this.Start();
+            m_output = this.StandardOutput;
+            this.WaitForExit();
+        }
+    }
+}
