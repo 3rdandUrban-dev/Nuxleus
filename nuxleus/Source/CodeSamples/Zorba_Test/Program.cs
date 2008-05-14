8e801765e64269f6879d48da32aee01aa76f4759
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2008-05-14 21:09:36 +0000

    added Zorba processor and console app test

---
+++ nuxleus/Source/CodeSamples/Zorba_Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nuxleus.Process;
+using System.IO;
+using System.Xml;
+
+namespace Zorba_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ZorbaProcess process = new ZorbaProcess();
+            process.RunQuery(args[0]);
+            XmlReader xReader = XmlReader.Create(new StringReader(process.Output.ReadToEnd()));
+            do
+            {
+                if(xReader.IsStartElement())
+                    Console.WriteLine(xReader.ReadOuterXml());
+            } while (xReader.Read());
+        }
+    }
+}
--- nuxleus/Source/Nuxleus.Process/Process/ZorbaProcess.cs
+++ nuxleus/Source/Nuxleus.Process/Process/ZorbaProcess.cs
@@ -11,7 +11,6 @@ namespace Nuxleus.Process
     public class ZorbaProcess : System.Diagnostics.Process
     {
         TextWriter m_logWriter;
-        ZorbaProcess m_proc;
         StreamReader m_output;
 
         [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
@@ -22,23 +21,25 @@ namespace Nuxleus.Process
         }
 
         [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
-        public ZorbaProcess(string parameters, TextWriter logWriter)
+        public ZorbaProcess(TextWriter logWriter)
         {
             m_logWriter = logWriter;
-            m_proc = new ZorbaProcess();
-            m_proc.StartInfo.FileName = "zorba";
+            base.StartInfo.FileName = "zorba";
 
         }
 
         public TextWriter LogWriter { get { return m_logWriter; } set { m_logWriter = value; } }
+        public StreamReader Output { get { return m_output; } set { m_output = value; } }
 
-
-        public void RunQuery(string fullPath)
+        public void RunQuery(string args)
         {
-            this.StartInfo.Arguments = "to be added";
-            this.Start();
-            m_output = this.StandardOutput;
-            this.WaitForExit();
+            base.StartInfo.Arguments = args;
+            base.StartInfo.RedirectStandardOutput = true;
+            base.StartInfo.UseShellExecute = false;
+            base.Start();
+            m_output = base.StandardOutput;
+            base.WaitForExit();
         }
+
     }
 }
