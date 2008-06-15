3a5c00b0f58483a0eb47ce022c768318ae379d54
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2008-06-15 08:27:35 +0000

    Forgot that I could declare the type directly given the use of generics which allows the ability to invoke the methods directly

--- nuxleus/Source/CodeSamples/SaxonExtensionFunction_Test/Program.cs
+++ nuxleus/Source/CodeSamples/SaxonExtensionFunction_Test/Program.cs
@@ -46,9 +46,12 @@ namespace GenericTypeOperation {
             Console.WriteLine("Starting generic object creation and method invocation test.");
             stopwatch.Start();
             for (int i = 0; i <= count; i++) {
-                object obj1 = ExtensionFunction<Test>.Create();
-                object obj2 = ExtensionFunction<Test>.Create("Created with a single string parameter");
-                object obj3 = ExtensionFunction<Test>.Create("Created with two", "string parameters");
+                Test obj1 = ExtensionFunction<Test>.Create();
+                Test obj2 = ExtensionFunction<Test>.Create("Created with a single string parameter");
+                Test obj3 = ExtensionFunction<Test>.Create("Created with two", "string parameters");
+                //Console.WriteLine("Test.GetOutput(\"test1\") returned: {0}", obj1.GetOutput("test1"));
+                //Console.WriteLine("Test.GetOutput(\"test2\") returned: {0}", obj2.GetOutput("test2"));
+                //Console.WriteLine("Test.GetOutput(\"test3\") returned: {0}", obj3.GetOutput("test3"));
                 Console.WriteLine("Test.GetOutput(\"test1\") returned: {0}", ExtensionFunction<Test>.Invoke<String>(obj1, "GetOutput", "test1"));
                 Console.WriteLine("Test.GetOutput(\"test2\") returned: {0}", ExtensionFunction<Test>.Invoke<String>(obj2, "GetOutput", "test2"));
                 Console.WriteLine("Test.GetOutput(\"test3\") returned: {0}", ExtensionFunction<Test>.Invoke<String>(obj3, "GetOutput", "test3"));
