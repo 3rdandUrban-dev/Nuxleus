a4bd36e94beb983c24fe00137771c485afb1f16f
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2008-06-15 07:34:55 +0000

    updated the test to push things a bit harder

--- nuxleus/Source/CodeSamples/SaxonExtensionFunction_Test/Program.cs
+++ nuxleus/Source/CodeSamples/SaxonExtensionFunction_Test/Program.cs
@@ -1,14 +1,54 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace GenericTypeOperation {
+
     class Program {
+
+        static long directEllapsedTime = 0;
+        static long genericsEllapsedTime = 0;
+
         static void Main(string[] args) {
-            object obj1 = ExtensionFunction<Test>.Create();
-            object obj2 = ExtensionFunction<Test>.Create("Created with a single string parameter");
-            object obj3 = ExtensionFunction<Test>.Create("Created with two", "string parameters");
-            Console.WriteLine("Test.GetOutput(\"test1\") returned: {0}", ExtensionFunction<Test>.Invoke<String>(obj1, "GetOutput", "test1"));
-            Console.WriteLine("Test.GetOutput(\"test2\") returned: {0}", ExtensionFunction<Test>.Invoke<String>(obj2, "GetOutput", "test2"));
-            Console.WriteLine("Test.GetOutput(\"test3\") returned: {0}", ExtensionFunction<Test>.Invoke<String>(obj3, "GetOutput", "test3"));
+            Stopwatch stopwatch = new Stopwatch();
+            int count = 100;
+            TestDirect(count, stopwatch);
+            TestGenerics(count, stopwatch);
+            Console.WriteLine("Completed direct test in:\t {0}ms", directEllapsedTime);
+            Console.WriteLine("Completed generics test in:\t {0}ms", genericsEllapsedTime);
+        }
+
+        public static void TestDirect(int count, Stopwatch stopwatch){
+            Console.WriteLine("Starting direct object creation and method invocation test.");
+            stopwatch.Start();
+            for (int i = 0; i <= count; i++) {
+                Test obj1 = new Test();
+                Test obj2 = new Test("Created with a single string parameter");
+                Test obj3 = new Test("Created with two", "string parameters");
+                Console.WriteLine("Test.GetOutput(\"test1\") returned: {0}", obj1.GetOutput("test1"));
+                Console.WriteLine("Test.GetOutput(\"test2\") returned: {0}", obj2.GetOutput("test2"));
+                Console.WriteLine("Test.GetOutput(\"test3\") returned: {0}", obj3.GetOutput("test3"));
+            }
+            stopwatch.Stop();
+            directEllapsedTime = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine("Complete!");
+            stopwatch.Reset();
+        }
+
+        public static void TestGenerics(int count, Stopwatch stopwatch) {
+            Console.WriteLine("Starting generic object creation and method invocation test.");
+            stopwatch.Start();
+            for (int i = 0; i <= count; i++) {
+                object obj1 = ExtensionFunction<Test>.Create();
+                object obj2 = ExtensionFunction<Test>.Create("Created with a single string parameter");
+                object obj3 = ExtensionFunction<Test>.Create("Created with two", "string parameters");
+                Console.WriteLine("Test.GetOutput(\"test1\") returned: {0}", ExtensionFunction<Test>.Invoke<String>(obj1, "GetOutput", "test1"));
+                Console.WriteLine("Test.GetOutput(\"test2\") returned: {0}", ExtensionFunction<Test>.Invoke<String>(obj2, "GetOutput", "test2"));
+                Console.WriteLine("Test.GetOutput(\"test3\") returned: {0}", ExtensionFunction<Test>.Invoke<String>(obj3, "GetOutput", "test3"));
+            }
+            stopwatch.Stop();
+            genericsEllapsedTime = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine("Complete!");
+            stopwatch.Reset();
         }
     }
 }
