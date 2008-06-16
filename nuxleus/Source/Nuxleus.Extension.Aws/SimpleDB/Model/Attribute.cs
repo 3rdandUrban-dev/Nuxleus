67ae97bcba1de7eb03f748d352cef7e4c5309896
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2008-06-16 17:27:45 +0000

    not sure if this made it or not

---
+++ nuxleus/Source/Nuxleus.Extension.Aws/SimpleDB/Model/Attribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Nuxleus.Extension.AWS.SimpleDB.Model {
+
+    [XmlTypeAttribute(Namespace = "http://sdb.amazonaws.com/doc/2007-11-07/")]
+    [XmlRootAttribute(Namespace = "http://sdb.amazonaws.com/doc/2007-11-07/", IsNullable = false)]
+    public struct Attribute {
+
+        string m_name;
+        string m_value;
+        static bool m_replace = false;
+
+        [XmlElementAttribute(ElementName = "Name")]
+        public string Name { get { return m_name; } set { m_name = value; } }
+
+        [XmlElementAttribute(ElementName = "Value")]
+        public string Value { get { return m_value; } set { m_value = value; } }
+
+        [XmlElementAttribute(ElementName = "Replace")]
+        public bool Replace { get { return m_replace; } set { m_replace = value; } }
+
+        public Attribute(string name, string value) {
+            m_name = name;
+            m_value = value;
+        }
+
+        public Attribute(string name, string value, bool replace) {
+            m_name = name;
+            m_value = value;
+            m_replace = replace;
+        }
+    }
+}
