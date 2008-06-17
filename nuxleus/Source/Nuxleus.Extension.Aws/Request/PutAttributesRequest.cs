7b9b253fa6b21251d7e34cc4401befe2f488acfc
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2008-06-17 23:33:26 +0000

    forgot to add this

---
+++ nuxleus/Source/Nuxleus.Extension.Aws/Request/PutAttributesRequest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nuxleus.Extension.AWS.SimpleDB;
+using System.Xml.Linq;
+using Nuxleus.MetaData;
+
+namespace Nuxleus.Extension.AWS.SimpleDB {
+
+    public struct PutAttributesRequest : IRequest {
+
+        String m_requestBody;
+
+        #region IRequest Members
+
+        public KeyValuePair<string, string>[] Headers {
+            get {
+                return 
+                    new KeyValuePair<string, string>[] {
+                        new KeyValuePair<string,string>("SOAPAction", LabelAttribute.FromMember(RequestType)),
+                    };
+            }
+        }
+
+        public RequestType RequestType {
+            get {
+                return RequestType.PutAttributes;
+            }
+        }
+
+        public String RequestMessage {
+            get {
+                return m_requestBody;
+            }
+            set {
+                m_requestBody = value;
+            }
+        }
+
+        #endregion
+    }
+}
