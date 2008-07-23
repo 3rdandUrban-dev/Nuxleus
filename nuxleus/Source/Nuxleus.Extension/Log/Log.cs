40c2d41a7e9e90b1712196b82622d0d2b559e97f
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2008-07-23 22:30:02 +0000

    provides logging capabilities to all objects

--- nuxleus/Source/Nuxleus.Extension/Log/Log.cs
+++ nuxleus/Source/Nuxleus.Extension/Log/Log.cs
@@ -5,8 +5,11 @@ using System.Text;
 using log4net;
 using log4net.Config;
 using log4net.Core;
+using System.Security.Permissions;
 
-namespace Nuxleus.Extension.Log {
+namespace Nuxleus.Extension {
+
+    [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.AllFlags)]
     public static class Log {
 
         static bool IsConfigured;
@@ -17,10 +20,27 @@ namespace Nuxleus.Extension.Log {
             return LogManager.GetLogger(obj.GetType());
         }
 
+        public static ILog GetLogger<T>() {
+            if (!IsConfigured)
+                Config();
+            return LogManager.GetLogger(typeof(T));
+        }
+
         private static ILog Logger(object obj) {
             if(!IsConfigured) 
                 Config();
             return LogManager.GetLogger(obj.GetType());
+        }
+
+        private static ILog Logger<T>() {
+            if (!IsConfigured)
+                Config();
+            return LogManager.GetLogger(typeof(T));
+        }
+
+        public static void Config(string logFile) {
+            XmlConfigurator.Configure(new System.IO.FileInfo((logFile)));
+            IsConfigured = true;
         }
 
         private static void Config() {
@@ -44,6 +64,22 @@ namespace Nuxleus.Extension.Log {
             Logger(obj).DebugFormat(format, args);
         }
 
+        public static void LogDebug<T>(object message, Exception exception) {
+            Logger<T>().Debug(message, exception);
+        }
+
+        public static void LogDebug<T>(object message) {
+            Logger<T>().Debug(message);
+        }
+
+        public static void LogDebug<T>(IFormatProvider provider, string format, params object[] args) {
+            Logger<T>().DebugFormat(provider, format, args);
+        }
+
+        public static void LogDebug<T>(string format, params object[] args) {
+            Logger<T>().DebugFormat(format, args);
+        }
+
         public static void LogError(this object obj, object message, Exception exception) {
             Logger(obj).Error(message, exception);
         }
@@ -58,6 +94,22 @@ namespace Nuxleus.Extension.Log {
 
         public static void LogError(this object obj, string format, params object[] args) {
             Logger(obj).ErrorFormat(format, args);
+        }
+
+        public static void LogError<T>(object message, Exception exception) {
+            Logger<T>().Error(message, exception);
+        }
+
+        public static void LogError<T>(object message) {
+            Logger<T>().Error(message);
+        }
+
+        public static void LogError<T>(IFormatProvider provider, string format, params object[] args) {
+            Logger<T>().ErrorFormat(provider, format, args);
+        }
+
+        public static void LogError<T>(string format, params object[] args) {
+            Logger<T>().ErrorFormat(format, args);
         }
 
         public static void LogFatal(this object obj, object message, Exception exception) {
@@ -76,6 +128,22 @@ namespace Nuxleus.Extension.Log {
             Logger(obj).FatalFormat(format, args);
         }
 
+        public static void LogFatal<T>(object message, Exception exception) {
+            Logger<T>().Fatal(message, exception);
+        }
+
+        public static void LogFatal<T>(object message) {
+            Logger<T>().Fatal(message);
+        }
+
+        public static void LogFatal<T>(IFormatProvider provider, string format, params object[] args) {
+            Logger<T>().FatalFormat(provider, format, args);
+        }
+
+        public static void LogFatal<T>(string format, params object[] args) {
+            Logger<T>().FatalFormat(format, args);
+        }
+
         public static void LogInfo(this object obj, object message, Exception exception) {
             Logger(obj).Info(message, exception);
         }
@@ -90,6 +158,22 @@ namespace Nuxleus.Extension.Log {
 
         public static void LogInfo(this object obj, string format, params object[] args) {
             Logger(obj).InfoFormat(format, args);
+        }
+
+        public static void LogInfo<T>(object message, Exception exception) {
+            Logger<T>().Info(message, exception);
+        }
+
+        public static void LogInfo<T>(object message) {
+            Logger<T>().Info(message);
+        }
+
+        public static void LogInfo<T>(IFormatProvider provider, string format, params object[] args) {
+            Logger<T>().InfoFormat(provider, format, args);
+        }
+
+        public static void LogInfo<T>(string format, params object[] args) {
+            Logger<T>().InfoFormat(format, args);
         }
 
         public static void LogWarn(this object obj, object message, Exception exception) {
@@ -108,5 +192,20 @@ namespace Nuxleus.Extension.Log {
             Logger(obj).WarnFormat(format, args);
         }
 
+        public static void LogWarn<T>(object message, Exception exception) {
+            Logger<T>().Warn(message, exception);
+        }
+
+        public static void LogWarn<T>(object message) {
+            Logger<T>().Warn(message);
+        }
+
+        public static void LogWarn<T>(IFormatProvider provider, string format, params object[] args) {
+            Logger<T>().WarnFormat(provider, format, args);
+        }
+
+        public static void LogWarn<T>(string format, params object[] args) {
+            Logger<T>().WarnFormat(format, args);
+        }
     }
 }
