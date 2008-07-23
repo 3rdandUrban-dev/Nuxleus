eb6d7ed6a5340ae08c6df8042e199fb66ed25020
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2008-07-23 22:17:23 +0000

    first take at a SimpleDB console app.  This is an adaptation of the IronPython SuperConsole code base

---
+++ nuxleus/Source/ConsoleApps/SimpleDBConsole/Operation/IOperation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleDBConsole {
+    public interface IOperation {
+        string Command { get; }
+        List<string> Invoke();
+    }
+}
---
+++ nuxleus/Source/ConsoleApps/SimpleDBConsole/Operation/ListDomains.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleDBConsole {
+    public struct ListDomains : IOperation {
+        public string m_command;
+        public string Command { get { return m_command; } }
+
+        public ListDomains(string command) {
+            m_command = command;
+        }
+        public List<string> Invoke() {
+            List<string> list = new List<string>();
+            list.Add(Command);
+            return list;
+        }
+    }
+}
