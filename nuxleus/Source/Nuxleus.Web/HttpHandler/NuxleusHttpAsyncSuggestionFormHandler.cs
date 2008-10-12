3657bfa434063583796fbae4f9be0209803479be
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2008-10-12 17:40:30 +0000

    updated to be a bit more efficient with its use of streams

--- nuxleus/Source/Nuxleus.Web/HttpHandler/NuxleusHttpAsyncSuggestionFormHandler.cs
+++ nuxleus/Source/Nuxleus.Web/HttpHandler/NuxleusHttpAsyncSuggestionFormHandler.cs
@@ -3,32 +3,36 @@
 // Please see http://creativecommons.org/licenses/by/3.0/us/ for specific detail.
 
 using System;
-using System.IO;
-using System.Xml;
-using System.Web;
-using System.Text;
-using System.Collections.Specialized;
 using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Text;
+using System.Web;
 using Amazon.SimpleDB;
 using Amazon.SimpleDB.Model;
-using Nuxleus.Agent;
 using Nuxleus.Web.HttpApplication;
-using System.Collections.Generic;
+using Nuxleus.Atom;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Nuxleus.Web.HttpHandler {
 
     public struct NuxleusHttpAsyncSuggestionFormHandler : IHttpAsyncHandler {
 
         FileStream m_file;
-        static long m_position = 0;
+        HttpResponse m_response;
         static object m_lock = new object();
+        static XmlSerializer m_xSerializer = new XmlSerializer(typeof(Entry));
+        static Encoding m_encoding = new UTF8Encoding();
 
         AmazonSimpleDBClient m_amazonSimpleDBClient;
         PutAttributes m_putAttributes;
 
         PledgeCount m_pledgeCount;
 
-        public void ProcessRequest ( HttpContext context ) {
+        public void ProcessRequest(HttpContext context) {
             //not called
         }
 
@@ -36,16 +40,12 @@ namespace Nuxleus.Web.HttpHandler {
             get { return false; }
         }
 
-        public IAsyncResult BeginProcessRequest ( HttpContext context, AsyncCallback cb, object extraData ) {
+        public IAsyncResult BeginProcessRequest(HttpContext context, AsyncCallback cb, object extraData) {
 
             HttpRequest request = context.Request;
-            HttpResponse response = context.Response;
+            m_response = context.Response;
 
             NameValueCollection form = request.Form;
-
-            //for (int x = 0; x < form.Count; x++ ) {
-            //    Console.WriteLine("Param: {0}", form[x]);
-            //}
 
             string name = form.Get("name");
             string slug = form.Get("slug");
@@ -53,6 +53,15 @@ namespace Nuxleus.Web.HttpHandler {
             string base_path = form.Get("base_path");
             string title = form.Get("title");
             string ip = context.Request.UserHostAddress.ToString();
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(String.Format("<p xmlns=\"http://www.w3.org/1999/xhtml\">{0}</p>", form.Get("suggestion")));
+
+            XHTMLBody body = new XHTMLBody { Elements = new XmlNode[] { doc.DocumentElement } };
+            Content content = new Content { Div = body, Type = "xhtml" };
+
+            Entry entry = new Entry { Content = content, Published = DateTime.Now, Updated = DateTime.Now, Title = title };
+
+            Console.WriteLine("Published: {0}, Updated: {1}, Title: {2}, Content: {3}", entry.Published, entry.Updated, entry.Title, entry.Content.Text);
 
             m_amazonSimpleDBClient = (AmazonSimpleDBClient)context.Application["simpledbclient"];
             m_pledgeCount = (PledgeCount)context.Application["pledgeCount"];
@@ -71,23 +80,45 @@ namespace Nuxleus.Web.HttpHandler {
             //    );
 
             //pledgeQueue.Enqueue(title);
+            
+            byte[] output;
+            using (MemoryStream stream = new MemoryStream()) {
+                stream.Seek(0, 0);
+                using (XmlWriter writer = new XmlTextWriter(stream, Encoding.Unicode)) {
+                    m_xSerializer.Serialize(writer, (Entry)entry);
+                    output = stream.GetBuffer();
+                    char[] chars = m_encoding.GetChars(Encoding.Convert(Encoding.Unicode, Encoding.UTF8, output, 0, output.Length));
+                    
 
-            string pledge = String.Format("<pledge time='{0}' email='{1}'><name>{2}</name><location>{3}</location><zip>{4}</zip></pledge>\r\n", DateTime.Now, slug, name, title, base_uri);
+                    using (TextWriter sWriter = Console.Out) {
+                        sWriter.Write(chars, 0, chars.Length);
+                    }
+                    using (StreamWriter sWriter = new StreamWriter(m_response.OutputStream, Encoding.UTF8)) {
+                        sWriter.Write(chars, 0, chars.Length);
+                    }
 
-            byte[] output = Encoding.ASCII.GetBytes(pledge);
-            lock (m_lock) {
-                m_file = new FileStream(request.MapPath("~/App_Data/TrackerLog.xml"), FileMode.OpenOrCreate, FileAccess.Write, FileShare.Write, 1024, true);
-                m_file.Seek(m_position, SeekOrigin.Begin);
-                m_position += output.Length;
-                return m_file.BeginWrite(output, 0, output.Length, cb, extraData);
+                    lock (m_lock) {
+                        string basePath = request.MapPath(String.Format("~{0}", base_path));
+                        Console.WriteLine(basePath);
+                        if (!Directory.Exists(basePath)) {
+                            Directory.CreateDirectory(basePath);
+                        }
+                        m_file = new FileStream(String.Format("{0}/{1}.xml", basePath, Guid.NewGuid()), FileMode.Create, FileAccess.Write, FileShare.ReadWrite, 1024, true);
+                        m_file.Seek(0, 0);
+                        return m_file.BeginWrite(output, 0, output.Length, cb, extraData);
+                    }
+                }
             }
         }
 
-        public void EndProcessRequest ( IAsyncResult result ) {
+        public void EndProcessRequest(IAsyncResult result) {
+            m_response.ContentType = "text/xml";
+            m_file.Close();
+            m_response.OutputStream.Close();
             //m_amazonSimpleDBClient.PutAttributes(m_putAttributes);
         }
 
-        private ReplaceableAttribute createReplacableAttribute ( string name, string value, bool replacable ) {
+        private ReplaceableAttribute createReplacableAttribute(string name, string value, bool replacable) {
             ReplaceableAttribute replacableAttribute = new ReplaceableAttribute();
             replacableAttribute.Name = name;
             replacableAttribute.Value = value;
