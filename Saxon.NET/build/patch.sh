6ee190704222fe2b7b42c98b812f6847a95030e7
xmlhacker <xmlhacker@83d55c6b-2440-4c65-b031-4153121aa5aa>
2009-12-19 08:04:39 +0000

    replacing with a new build system

--- Saxon.NET/build/patch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-patch Saxon/net/sf/saxon/Configuration.java < Configuration.java.patch
--- Saxon.NET/build/prep.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf Saxon/net/sf/saxon/ant Saxon/net/sf/saxon/xqj Saxon/net/sf/saxon/s9api Saxon/net/sf/saxon/pull/PullToStax.java Saxon/net/sf/saxon/pull/StaxBridge.java
-find Saxon/ -name *.java > allsources.lst
