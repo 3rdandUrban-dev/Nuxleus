45a7db9d18f7d06af12266f4583407dcd9145f3d
M. David Peterson <m.david@3rdandurban.com>
2011-09-28 04:33:45 -0600

    Edited src/Nuxleus.Core/Core/Singleton/SingletonProvider.cs via GitHub

--- src/Nuxleus.Core/Core/Singleton/SingletonProvider.cs
+++ src/Nuxleus.Core/Core/Singleton/SingletonProvider.cs
@@ -2,20 +2,10 @@ using System;
 
 namespace Nuxleus.Core
 {
-<<<<<<< HEAD
 	public class SingletonProvider <T> where T:new()
 	{
 		SingletonProvider ()
 		{
-=======
-	public class SingletonProvider <T> where T: class, new()
-	{
-		
-		
-		SingletonProvider ()
-		{
-			
->>>>>>> 1c9f7b30d9b16dbd6968aad395edf261f5973dad
 		}
 
 		public static T Instance {
@@ -24,30 +14,11 @@ namespace Nuxleus.Core
 
 		class SingletonCreator
 		{
-<<<<<<< HEAD
 			static SingletonCreator ()
 			{
 			}
 
 			internal static readonly T instance = new T ();
-=======
-			static Dictionary<string,string > contentTypes = new Dictionary<string, string> ();
-			
-			static SingletonCreator (params KeyValuePair<string,string> contentTypeParams)
-			{
-				
-
-				foreach (KeyValuePair<string,string> contentType in contentTypeParams) {
-					contentTypes [contentType.Key] = contentType.Value;
-				
-				}
-			}
-			
-			internal static readonly T instance = new T (contentTypes);
->>>>>>> 1c9f7b30d9b16dbd6968aad395edf261f5973dad
 		}
 	}
-	
-
 }
-
